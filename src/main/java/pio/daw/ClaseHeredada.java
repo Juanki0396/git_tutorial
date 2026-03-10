3ba6dd359a8aa31f4202ef06e77ca2f39193e46f
javiport16 <javiport16@gmail.com>
2026-03-10 09:52:50 +0100

    cambios2

--- src/main/java/pio/daw/ClaseHeredada.java
+++ src/main/java/pio/daw/ClaseHeredada.java
@@ -1,3 +1,4 @@
+package pio.daw;
 public class ClaseHeredada extends AbstractItem {
 public ClaseHeredada(String name, String id, String description, String color){
     super (name, id, description);
