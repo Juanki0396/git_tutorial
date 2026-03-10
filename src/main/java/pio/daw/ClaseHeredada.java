3431b8f9416b3b73553f1b8efdcf4b0be857f2a1
lucia <luciamoneoo@gmail.com>
2026-03-10 09:45:07 +0100

    Cambios rama heredada

---
+++ src/main/java/pio/daw/ClaseHeredada.java
@@ -0,0 +1,8 @@
+package pio.daw;
+
+public class ClaseHeredada extends AbstractItem {
+    public ClaseHeredada(String name, String id, String description, String color){
+        super (name, id, description);
+        this.color = color;
+    }
+}
