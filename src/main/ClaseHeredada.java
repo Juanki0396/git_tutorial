3d4544e16fb4f2e7969ba57c259dd913d1928cf9
javiport16 <javiport16@gmail.com>
2026-03-10 09:50:49 +0100

    cambios1

---
+++ src/main/ClaseHeredada.java
@@ -0,0 +1,6 @@
+public class ClaseHeredada extends AbstractItem {
+public ClaseHeredada(String name, String id, String description, String color){
+    super (name, id, description);
+    this.color = color;
+}
+}
